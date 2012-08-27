ab9193d433316e0bbad12b1fd6881748b5a27b69
Brad Parbs <brad@bradparbs.com>
2012-08-26 23:53:27 -0500

    fix all the bugs!
    fixed short code output
    updated js path
    updated version number

--- init.php
+++ init.php
@@ -2,41 +2,43 @@
 /*
 Plugin Name: wpTextResize
 Description: Use the [wpResize] shortcode wherever you want the resizing controls to show up.
-Version: 1.2
+Version: 1.4
 Author: Brad Parbs
 Author URI: http://github.com/bradp
 */
 
 
 function wpTextResize(){
+$plugin_path = plugin_dir_url(__FILE__);
+
 wp_enqueue_script(
-  "text.js", WP_PLUGIN_URL."/wpTextResize/text.js");
+  "text.js", $plugin_path ."/text.js");
 }
 
 add_action('wp_head','wpTextResize',6);
 
 function wpTextResizeControlsWidget(){
-	?>
-	<div title='Resize Body Text' class="wpTextResizeControls" style='background-color:lightgray;border-radius:7px;-moz-border-radius:7px;-webkit-border-radius:7px;float:right;clear:right;padding:3px;'>
-		<a class='increaseFont' title='Increase Font Size' style='font-size:18px;'>A</a><a class='resetFont' title='Reset Font Size' style='font-size:13px;padding-left:10px;'>A</a><a class='decreaseFont' title='Decrease Font Size' style='font-size:10px;padding-left:10px;'>A</a>
-	<?php
+	$widget = "
+	<div title='Resize Body Text' class='wpTextResizeControls' style='background-color:lightgray;border-radius:7px;-moz-border-radius:7px;-webkit-border-radius:7px;padding:3px;width:55px;'>
+		<a class='increaseFont' title='Increase Font Size' style='font-size:18px;'>A</a><a class='resetFont' title='Reset Font Size' style='font-size:13px;padding-left:10px;'>A</a><a class='decreaseFont' title='Decrease Font Size' style='font-size:10px;padding-left:10px;'>A</a></div>";
+	return $widget;
 }
 
 function wpTextResizeControls($custom){
 if($custom == 1 ){
 	?>
 	<div title='Resize Body Text' class="wpTextResizeControls">
-	<a class='increaseFont' title='Increase Font Size'>A</a><a class='resetFont' title='Reset Font Size'>A</a><a class='decreaseFont' title='Decrease Font Size'>A</a>
+	<a class='increaseFont' title='Increase Font Size'>A</a><a class='resetFont' title='Reset Font Size'>A</a><a class='decreaseFont' title='Decrease Font Size'>A</a></div>
 	<?php
 }
 
 else{
 	?>
-	<div title='Resize Body Text' class="wpTextResizeControls" style='background-color:lightgray;border-radius:7px;-moz-border-radius:7px;-webkit-border-radius:7px;float:right;clear:right;padding:3px;'>
+	<div title='Resize Body Text' class="wpTextResizeControls" style='background-color:lightgray;border-radius:7px;-moz-border-radius:7px;-webkit-border-radius:7px;padding:3px;width:55px;'>
 		<a class='increaseFont' title='Increase Font Size' style='font-size:18px;'>A</a><a class='resetFont' title='Reset Font Size' style='font-size:13px;padding-left:10px;'>A</a><a class='decreaseFont' title='Decrease Font Size' style='font-size:10px;padding-left:10px;'>A</a>
 	</div>	
 	<?
 	}
 }
 
-add_shortcode('wpResize','wpTextResizeControlsWidget')
+add_shortcode('wpResize','wpTextResizeControlsWidget');
--- readme.txt
+++ readme.txt
@@ -7,10 +7,10 @@ Tested up to: 3.3.2
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-wpTextResizeControls is an easy to use template tag to generate Increase, Decrease, reset font size controls for body text on a WordPress site.
+wpTextResize is an easy to use template tag to generate Increase, Decrease, reset font size controls for body text on a WordPress site.
 
 == Description ==
-wpTextResizeControls is an easy to use template tag to generate Increase, Decrease, reset font size controls for body text on a WordPress site.
+wpTextResize is an easy to use template tag to generate Increase, Decrease, reset font size controls for body text on a WordPress site.
 
 Just use the [wpResize] shortcode wherever you want the controls to show up.
 
@@ -20,17 +20,26 @@ Go wild!
 
 == Installation ==
 
-This section describes how to install the plugin and get it working.
-
-e.g.
-
 1. Upload the plugin to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Add wpTextResizeControls(0) to your template for the controls to be automatically styled, or wpTextResizeControls(1) to manually style. When manually styling, wpTextResizeControls is the container, and increaseFont, resetFont, and decreaseFont are all anchors you can target.
+3. Add the [wpResize] shortcode wherever you want the controls to appear.
+4. OR Add wpTextResizeControls(0) to your template for the controls to be automatically styled, or wpTextResizeControls(1) to manually style. When manually styling, wpTextResizeControls is the container, and increaseFont, resetFont, and decreaseFont are all anchors you can target.
 
 == Changelog ==
+
+= 1.4 = 
+
+* Fixed issues with shortcode output
+* Better coding with returning shortcode instead of echoing
+* updated js path in plugin in case the user renames the plugin folder
+* updated version number
+
 
 = 1.2 =
 
 * Added resizing ability to all WordPress themes, no matter what html layout exists
 * added shortcode to display controls
+
+= 1.0 =
+
+* initial release
