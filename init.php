a5a775832c8f2aa09b25ee27bb8ba57072df3017
Brad Parbs <Brad@bradparbs.com>
2012-08-27 20:24:24 -0500

    Added widget and hand cursors for controls

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: wpTextResize
 Description: Use the [wpResize] shortcode wherever you want the resizing controls to show up.
-Version: 1.4
+Version: 1.5
 Author: Brad Parbs
 Author URI: http://github.com/bradp
 */
@@ -26,7 +26,7 @@ function wpTextResizeBodyClass($classes) {
 function wpTextResizeControlsWidget(){
 	$widget = "
 	<div title='Resize Body Text' class='wpTextResizeControls' style='background-color:lightgray;border-radius:7px;-moz-border-radius:7px;-webkit-border-radius:7px;padding:3px;width:55px;'>
-		<a class='increaseFont' title='Increase Font Size' style='font-size:18px;'>A</a><a class='resetFont' title='Reset Font Size' style='font-size:13px;padding-left:10px;'>A</a><a class='decreaseFont' title='Decrease Font Size' style='font-size:10px;padding-left:10px;'>A</a></div>";
+		<a class='increaseFont' title='Increase Font Size' style='font-size:18px;cursor:pointer;' >A</a><a class='resetFont' title='Reset Font Size' style='font-size:13px;padding-left:10px;cursor:pointer;'>A</a><a class='decreaseFont' title='Decrease Font Size' style='font-size:10px;padding-left:10px;cursor:pointer;'>A</a></div>";
 	return $widget;
 }
 
@@ -41,11 +41,54 @@ if($custom == 1 ){
 else{
 	?>
 	<div title='Resize Body Text' class='wpTextResizeControls' style='background-color:lightgray;border-radius:7px;-moz-border-radius:7px;-webkit-border-radius:7px;padding:3px;width:55px;'>
-		<a class='increaseFont' title='Increase Font Size' style='font-size:18px;'>A</a><a class='resetFont' title='Reset Font Size' style='font-size:13px;padding-left:10px;'>A</a><a class='decreaseFont' title='Decrease Font Size' style='font-size:10px;padding-left:10px;'>A</a>
+		<a class='increaseFont' title='Increase Font Size' style='font-size:18px;cursor:pointer;'>A</a><a class='resetFont' title='Reset Font Size' style='font-size:13px;padding-left:10px;cursor:pointer;'>A</a><a class='decreaseFont' title='Decrease Font Size' style='font-size:10px;padding-left:10px;cursor:pointer;'>A</a>
 	</div>	
 	<?
 	}
 }
 
 add_shortcode('wpResize','wpTextResizeControlsWidget');
+
+class wpTextResize extends WP_Widget
+{
+  function wpTextResize()
+  {
+    $widget_ops = array('classname' => 'wpTextResize', 'description' => 'Adds text resizing controls' );
+    $this->WP_Widget('wpTextResize', 'wpTextResize', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+ 
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
+ 
+    wpTextResizeControls(0);
+
+    echo $after_widget;
+  }
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("wpTextResize");') );
+
 
